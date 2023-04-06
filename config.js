b375220c9b07b83e7300918d08bcf3ff1f99f77c
Cao Gia Nghi <88968067+DoubleBBBBB@users.noreply.github.com>
2023-04-06 09:41:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://github.com/DoubleBBBBB';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng em';
+const contentCard = ' Happy Birthday Make A Wish. Dear brother, hope you receive everything you wish for this year. You deserve the best! Thanks for being such agreat brother';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
